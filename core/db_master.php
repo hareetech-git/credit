2efdbd0acf0575ac8f1a72d8269a387b06801c78
hareetech-git <githareetech@gmail.com>
2026-01-31 14:10:42 +0530

    Update config

--- core/db_master.php
+++ core/db_master.php
@@ -1,21 +1,17 @@
 <?php
-// core/db_master.php
-
 if (
     $_SERVER['HTTP_HOST'] === 'localhost' ||
     $_SERVER['HTTP_HOST'] === '127.0.0.1'
 ) {
-    // LOCAL
-    $DB_HOST = "localhost";
-    $DB_USER = "root";
-    $DB_PASS = "";
-    $DB_NAME = "credit";
-    $DB_PORT = 3306;
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'credit');
+    define('DB_PORT', 3306);
 } else {
-    // PRODUCTION
-    $DB_HOST = "localhost";
-    $DB_USER = "u443392627_credit_loan";
-    $DB_PASS = "~Q9fm9|HZi8y"; 
-    $DB_NAME = "u443392627_credit_loan";
-    $DB_PORT = 3306;
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'u443392627_credit_loan');
+    define('DB_PASS', '~Q9fm9|HZi8y');
+    define('DB_NAME', 'u443392627_credit_loan');
+    define('DB_PORT', 3306);
 }
--- customer/db/config.php
+++ customer/db/config.php
@@ -1,14 +1,14 @@
 <?php
-require_once __DIR__ . '/../../core/db_master.php';
+require_once realpath(__DIR__ . '/../../core/db_master.php');
 
 $conn = mysqli_connect(
-    $DB_HOST,
-    $DB_USER,
-    $DB_PASS,
-    $DB_NAME,
-    $DB_PORT
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    DB_PORT
 );
 
 if (!$conn) {
-    die("Database connection failed");
+    die('Database connection failed');
 }
