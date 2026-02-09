db67c8c9cafa71dc0369c697f3e1594dc5a3badb
hareetech-git <githareetech@gmail.com>
2026-02-09 17:33:03 +0530

    Update DB

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2026 at 12:44 PM
+-- Generation Time: Feb 09, 2026 at 01:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
