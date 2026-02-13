fa32e51c1f7b87bb47ef8078862933cc8e738897
hareetech-git <githareetech@gmail.com>
2026-02-13 11:41:56 +0530

    smtp credential update

--- includes/smtp_config.php
+++ includes/smtp_config.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/app_env.php';
 return [
     'host' => uc_env('SMTP_HOST', 'smtp.hostinger.com'),
     'port' => (int)uc_env('SMTP_PORT', '587'),
-    'username' => uc_env('SMTP_USERNAME', 'sanayam@hareetech.com'),
-    'password' => uc_env('SMTP_PASSWORD', 'Hareetech@San25'),
+    'username' => uc_env('SMTP_USERNAME', 'info@udharcapital.com'),
+    'password' => uc_env('SMTP_PASSWORD', 'info@UdhaarCapital2025'),
     'encryption' => uc_env('SMTP_ENCRYPTION', 'tls'),
-    'from_address' => uc_env('SMTP_FROM_ADDRESS', 'sanayam@hareetech.com'),
+    'from_address' => uc_env('SMTP_FROM_ADDRESS', 'info@udharcapital.com'),
     'from_name' => uc_env('SMTP_FROM_NAME', 'Udhaar Capital'),
 ];
