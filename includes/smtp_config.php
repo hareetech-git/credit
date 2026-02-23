adce1769abf589d307f3218dc6dc235bfb3ed182
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-02-23 12:49:20 +0530

    update credential

--- includes/smtp_config.php
+++ includes/smtp_config.php
@@ -5,7 +5,7 @@ return [
     'host' => uc_env('SMTP_HOST', 'smtp.hostinger.com'),
     'port' => (int)uc_env('SMTP_PORT', '587'),
     'username' => uc_env('SMTP_USERNAME', 'info@udharcapital.com'),
-    'password' => uc_env('SMTP_PASSWORD', 'info@UdhaarCapital2025'),
+    'password' => uc_env('SMTP_PASSWORD', 'Udhar@2030'),
     'encryption' => uc_env('SMTP_ENCRYPTION', 'tls'),
     'from_address' => uc_env('SMTP_FROM_ADDRESS', 'info@udharcapital.com'),
     'from_name' => uc_env('SMTP_FROM_NAME', 'Udhaar Capital'),
